9e4d9b7471cd3868919e389c3a8992eac9ba4027
smile-yz <1149392628@qq.com>
2020-11-11 17:08:35 +0800

    yz修改了

--- duguqiujian/src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
+++ duguqiujian/src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
@@ -3,5 +3,6 @@ package com.atguigu.jianfa.dugujiujian;
 public class Dugu {
     public static void main(String[] args) {
         System.out.println("jianfa dugujiujian1!!!!!");
+        System.out.println("jianfa dugujiujian1!!!!!");
     }
 }
